74136241667ad51139a7dc976b3a9c0f3948d7d0
codewithghouse <personalghouse@gmail.com>
2026-01-02 19:59:02 +0530

    Fix CORS issue for Netlify deployment

--- server.js
+++ server.js
@@ -18,11 +18,11 @@ app.use(cors({
         'http://localhost:5173',
         'http://localhost:8080',
         'http://localhost:3000',
-        process.env.CLIENT_URL
-    ].filter(Boolean),
+        'https://expirioo.netlify.app'
+    ],
+    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 // Handle preflight requests
